2a727f24cfc4166513cf7c72127eb816d0282eea
Xander Byrne <ajnb3@cam.ac.uk>
2024-08-13 09:37:16 +0100

    Redesigning Fig 2

--- src/scripts/create_fig2_embedding.py
+++ src/scripts/create_fig2_embedding.py
@@ -82,7 +82,8 @@ def create_legend_handles():
                 }[cl_code],
                 MARKER_DF.loc[cl_code, "c"],
                 marker=MARKER_DF.loc[cl_code, "marker"],
-                markersize=10,
+                markersize=12,
+                markeredgewidth=2,
             )
         )
     return handles
@@ -94,21 +95,36 @@ if __name__ == "__main__":
     names = fl["names"]
     embedding = fl["embedding"]
 
-    fg, axs = plt.subplots(
-        2,
-        2,
-        figsize=(12, 12),
-        gridspec_kw={"wspace": 0.0, "hspace": 0.0},
-    )
-
     # -------------------
     # Agnostic plot
-    ax = axs[0, 0]
+    fg, ax = plt.subplots(1, 1, figsize=(7, 7))
     ax.scatter(embedding[:, 0], embedding[:, 1], c="k", s=8)
+    ax.set_xticks([])
+    ax.set_yticks([])
+    ax.set_xlabel("t-SNE 1", fontsize=20)
+    ax.set_ylabel("t-SNE 2", fontsize=20)
+
+    ax.annotate(
+        "(a)",
+        xy=(0.02, 0.94),
+        xycoords="axes fraction",
+        fontsize=20,
+    )
+
+    fg.tight_layout()
+    fg.savefig("../tex/figures/fig2a_embedding.pdf", dpi=300)
 
     # -------------------
     # Labelled plot
-    ax = axs[1, 0]
+
+    fg, axs = plt.subplots(
+        1,
+        3,
+        figsize=(14, 6),
+        gridspec_kw={"wspace": 0.0, "hspace": 0.0, "width_ratios": [1, 1, 0.05]},
+    )
+
+    ax = axs[0]
 
     fl = np.load("../data/coadded_spectra.npz", allow_pickle=True)
     names = fl["names"]
@@ -120,35 +136,45 @@ if __name__ == "__main__":
     for cl in np.unique(class_code):
         mask = class_code == cl
         marker = MARKER_DF.loc[cl]
+        if cl[0] == "D":
+            s = 15
+        else:
+            s = 20
         ax.scatter(
             embedding[mask, 0],
             embedding[mask, 1],
-            s=15,
+            s=s,
             c=marker["c"],
             marker=marker["marker"],
         )
 
     leg = ax.legend(
         handles=create_legend_handles(),
-        bbox_to_anchor=(1.01, 0.6),
-        fontsize=20,
-        ncol=2,
-        columnspacing=0.0,
+        bbox_to_anchor=(0.0, 1.022),
+        fontsize=17,
+        # ncol=2,
+        # columnspacing=0.0,
     )
     for i in [0, 7]:
         leg.get_texts()[i].set_weight("bold")  # Bold the headers
-        leg.get_texts()[i].set_position((-39, 0))  # Move the headers to the left
+        # leg.get_texts()[i].set_position((-20, 0))  # Move the headers to the left
+
+    ax.annotate(
+        "(b)",
+        xy=(0.02, 0.93),
+        xycoords="axes fraction",
+        fontsize=20,
+    )
 
     # -------------------
     # Temperature coding
-    ax = axs[0, 1]
+    ax = axs[1]
+
+    gf19 = pd.read_csv("../data/gf19.csv", index_col=0)
 
     teff_cmp = LinearSegmentedColormap.from_list(
         "temp", ["black", "red", "orange", "yellow", "yellow", "white"]
     )
-
-    gf19 = pd.read_csv("../data/gf19.csv", index_col=0)
-
     sc = ax.scatter(
         embedding[:, 0],
         embedding[:, 1],
@@ -157,9 +183,9 @@ if __name__ == "__main__":
         cmap=teff_cmp,
     )
 
-    cax = fg.add_axes([0.55, 0.47, 0.4, 0.03])
-    cbar = fg.colorbar(sc, cax=cax, orientation="horizontal", pad=0)
+    ax.annotate("(c)", xy=(0.02, 0.93), xycoords="axes fraction", fontsize=20)
 
+    cbar = fg.colorbar(sc, cax=axs[2])
     cbar.set_label(r"$T_\text{eff}\; [10^3\,\text{K}]$", fontsize=20)
     T_ticks = np.array(
         [
@@ -174,18 +200,17 @@ if __name__ == "__main__":
         ]
     )
     cbar.set_ticks(np.log10(T_ticks))
-    cbar.set_ticklabels([f"${int(T/1e3)}$" for T in T_ticks], fontsize=16)
+    cbar.set_ticklabels([f"${int(T/1e3)}\,000$" for T in T_ticks], fontsize=16)
 
     # -------------------
     # Aesthetic
-    for ax in axs.flatten():
+    for ax in axs[:2]:
         # ax.set_xlabel("t-SNE 1", fontsize=20)
         ax.set_xticks([])
         ax.set_yticks([])
-    axs[1, 1].axis("off")
 
     # -------------------
     # Saving
 
     fg.tight_layout()
-    fg.savefig("../tex/figures/fig2_embedding.pdf", dpi=300)
+    fg.savefig("../tex/figures/fig2b_embedding.pdf", dpi=300)
